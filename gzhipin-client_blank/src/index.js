599d55dddfeb6924fe5ab785a7ee0995d4500987
gyl327 <2950903389@qq.com>
2021-07-17 10:39:33 +0800

    引入redux

--- gzhipin-client_blank/src/index.js
+++ gzhipin-client_blank/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {HashRouter, Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom"
+import {Provider} from 'react-redux'
 
 import Register from './containers/register'
 import Login from './containers/login'
 import Main from './containers/main'
+import store from './redux/store'
 
 ReactDOM.render( (
-  <HashRouter>
-    <Switch>
-      <Route path='/register' component={Register}></Route>
-      <Route path='/login' component={Login}></Route>
-      <Route component={Main}></Route>
-    </Switch>
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route path='/register' component={Register}></Route>
+        <Route path='/login' component={Login}></Route>
+        <Route component={Main}></Route>
+      </Switch>
+    </HashRouter>
+  </Provider>
 ), document.getElementById('root'))
