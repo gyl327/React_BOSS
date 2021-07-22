ba4a2ebc0f0255434643f9618906f61b74048002
gyl327 <2950903389@qq.com>
2021-07-22 19:26:46 +0800

    完成了老板和大神的静态界面与路由组件

--- gzhipin-client_blank/src/containers/main/index.jsx
+++ gzhipin-client_blank/src/containers/main/index.jsx
@@ -3,12 +3,19 @@
 */
 
 import React, {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+import LaobanInfo from '../laoban-info'
+import DashenInfo from '../dashen-info'
 
 export default class Main extends Component {
   render() {
     return (
       <div>
-        Main
+        <Switch>
+          <Route path='/laobaninfo' component={LaobanInfo}/>
+          <Route path='/dasheninfo' component={DashenInfo}/>
+        </Switch>
       </div>
     )
   }
